66c59398a43fa56cfa458c9214db724a21ecd9b5
Adrien Berchet <adrien.berchet@epfl.ch>
2022-12-02 16:42:22 +0100

    Add function to get datasets

--- src/hdf.rs
+++ src/hdf.rs
@@ -11,6 +11,10 @@ pub fn get_subgroups(group: &hdf5::Group) -> Vec<hdf5::Group> {
     group.groups().unwrap()
 }
 
+pub fn get_datasets(group: &hdf5::Group) -> Vec<hdf5::Dataset> {
+    group.datasets().unwrap()
+}
+
 // pub fn print_subgroups(group: &hdf5::Group) {
 //     let groups = get_subgroups(group);
 //     for subgroup in groups {
