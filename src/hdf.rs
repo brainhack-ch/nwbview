fc08113d4e25fc04b0c116dd69456c959b28f299
Adrien Berchet <adrien.berchet@epfl.ch>
2022-12-02 14:56:33 +0100

    Lint

--- src/hdf.rs
+++ src/hdf.rs
@@ -1,26 +1,19 @@
 use hdf5::File;
 
 pub(crate) fn read_nwb_file(path: &str) -> Option<hdf5::File> {
-    let handler = match File::open(path) {
+    match File::open(path) {
         Err(_) => None,
         Ok(hdf_file) => Some(hdf_file),
-    };
-    return handler;
+    }
 }
 
 pub fn get_subgroups(group: &hdf5::Group) -> Vec<hdf5::Group> {
-    return group.groups().unwrap();
+    group.groups().unwrap()
 }
 
-pub fn print_subgroups(group: &hdf5::Group) {
-    // let mut all_subgroups: Vec<&hdf5::Group>;
-    // all_subgroups.append(group);
-    let groups = get_subgroups(&group);
-    // all_subgroups.append(groups);
-    for subgroup in groups {
-        println!("In get_subgroups for {}: {}", group.name(), subgroup.name());
-        get_subgroups(&subgroup);
-        // all_subgroups.append(subgroup);
-    }
-    // return groups;
-}
+// pub fn print_subgroups(group: &hdf5::Group) {
+//     let groups = get_subgroups(group);
+//     for subgroup in groups {
+//         println!("In get_subgroups for {}: {}", group.name(), subgroup.name());
+//     }
+// }
