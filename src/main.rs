68ed7f19f987406c97440eab2053ce64b7f3681c
Anil Tuncel <anil.tuncel@epfl.ch>
2023-02-07 21:38:37 +0100

    added icon

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,21 @@ mod table;
 use gui::NWBView;
 
 fn main() {
+    let icon = image::open("static/icon.png")
+        .expect("Failed to open icon path")
+        .to_rgba8();
+    let (icon_width, icon_height) = icon.dimensions();
+
     let options = eframe::NativeOptions {
+        icon_data: Some(eframe::IconData {
+            rgba: icon.into_raw(),
+            width: icon_width,
+            height: icon_height,
+        }),
         drag_and_drop_support: true,
         ..Default::default()
     };
+
     eframe::run_native(
         "NWB View",
         options,
