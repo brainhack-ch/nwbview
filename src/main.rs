224ff13cb932faa8981530cf352a5c7a87ddf5d4
anilbey <anil.tuncel@epfl.ch>
2023-03-14 20:12:32 +0100

    Release nwbview 0.2.4 (#35)
    * add back static/icon to include directive in Cargo.toml
    
    * version 0.2.4
    
    * remove extra call to eframe::run_native

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,18 @@ use image::GenericImageView;
 fn main() {
     const ICON: &[u8] = include_bytes!("../static/icon.png");
 
+    let mut options = eframe::NativeOptions {
+        drag_and_drop_support: true,
+        ..Default::default()
+    };
     match image::load_from_memory(ICON) {
+        Err(error) => {
+            println!("Error raised while loading icon: {}", error);
+            println!("Launching NWBView without the icon...");
+        }
         Ok(icon) => {
             let (icon_width, icon_height) = icon.dimensions();
-
-            let options = eframe::NativeOptions {
+            options = eframe::NativeOptions {
                 icon_data: Some(eframe::IconData {
                     rgba: icon.into_rgba8().to_vec(),
                     width: icon_width,
@@ -23,27 +30,12 @@ fn main() {
                 drag_and_drop_support: true,
                 ..Default::default()
             };
-
-            eframe::run_native(
-                "NWB View",
-                options,
-                Box::new(|_cc| Box::<NWBView>::default()),
-            )
-            .ok();
-        }
-        Err(error) => {
-            println!("Error raised while loading icon: {}", error);
-            println!("Launching without icon...");
-            let options = eframe::NativeOptions {
-                drag_and_drop_support: true,
-                ..Default::default()
-            };
-            eframe::run_native(
-                "NWB View",
-                options,
-                Box::new(|_cc| Box::<NWBView>::default()),
-            )
-            .ok();
         }
     }
+    eframe::run_native(
+        "NWB View",
+        options,
+        Box::new(|_cc| Box::<NWBView>::default()),
+    )
+    .ok();
 }
